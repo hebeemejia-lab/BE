bb5e2a61b21adcd1ea3afb99aba5f27406892f1c
Héber Mejía <hebeemejia.lab@gmail.com>
2026-01-29 22:40:51 -0400

    fix: allow Render frontend origin in CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,6 +33,7 @@ const allowedOrigins = [
   'https://bancoexclusivo.lat',
   'http://www.bancoexclusivo.lat',
   'http://bancoexclusivo.lat',
+  'https://be-2-3wc8.onrender.com',
 ];
 
 // Agregar FRONTEND_URL si está configurado y no está vacío
