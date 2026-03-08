1d32adfe950d56e6790129a880a922b076ea6d1b
Héber Mejía <hebeemejia.lab@gmail.com>
2026-03-08 15:21:02 -0400

    Fix: usa instancia correcta de sequelize en modelos CirculoAhorro y CirculoMiembro

--- backend/src/models/CirculoAhorro.js
+++ backend/src/models/CirculoAhorro.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('./index');
+const { sequelize } = require('../config/database');
 
 const CirculoAhorro = sequelize.define('CirculoAhorro', {
   nombre: { type: DataTypes.STRING, allowNull: false },
